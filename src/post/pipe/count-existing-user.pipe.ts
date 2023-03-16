5c159cfee81c953b5d0c1bb2ad73c29f201578d5
Matheus <math.av7@outlook.com>
2023-03-16 13:12:40 -0300

    feat<validation>: validating existing user on post creation

---
+++ src/post/pipe/count-existing-user.pipe.ts
@@ -0,0 +1,36 @@
+import {
+    BadRequestException,
+    HttpStatus,
+    Injectable,
+    PipeTransform
+} from '@nestjs/common';
+import * as Joi from 'joi';
+import { UserService } from 'src/user';
+import { CreatePostDto } from '../dto';
+
+@Injectable()
+export class CountExistingUserPipe implements PipeTransform {
+  constructor(private readonly userService: UserService) {}
+
+  async transform(value: CreatePostDto): Promise<CreatePostDto> {
+    const { authorId: id } = value;
+
+    const userCount = await this.userService.count({
+      where: {
+        id,
+      },
+    });
+
+    
+    Joi.assert(
+      userCount,
+      Joi.number().min(1).max(1),
+      new BadRequestException({
+        error: HttpStatus.BAD_REQUEST,
+        message: `Given user don't exists`,
+      }),
+      );
+
+    return value;
+  }
+}
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -9,9 +9,11 @@ import {
   Patch,
   Post,
   UseGuards,
+  UsePipes,
 } from '@nestjs/common';
 import { CreatePostDto, PostResponseDto, UpdatePostDto } from './dto';
 import { JWTAuthGuard } from './guards/jwt-auth.guard';
+import { CountExistingUserPipe } from './pipe';
 import { PostService } from './post.service';
 
 @Controller('posts')
@@ -19,6 +21,7 @@ import { PostService } from './post.service';
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @UsePipes(CountExistingUserPipe)
   @Post()
   public async create(@Body() input: CreatePostDto): Promise<PostResponseDto> {
     return await this.postService.create(input);
