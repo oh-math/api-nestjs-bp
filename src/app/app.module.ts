4aaf3c71592ce55c408d91f582e08c025977818d
Matheus <math.av7@outlook.com>
2023-04-19 01:10:34 -0300

    feature: file upload sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { PostModule } from 'src/post';
 import { UserModule } from 'src/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FileModule } from 'src/file';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [UserModule, PostModule, AuthModule, FileModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/file/file.controller.ts
@@ -0,0 +1,19 @@
+import {
+    Controller,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileService } from './file.service';
+
+@Controller('file')
+export class FileController {
+  constructor(private readonly fileService: FileService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {file}
+  }
+}
