813650cacd2b706540be8ebfd740714cf51b5b1b
Matheus <math.av7@outlook.com>
2023-04-21 21:35:08 -0300

    refactor: exclude file folder and creating a specific S3 folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { PostModule } from 'src/post';
 import { UserModule } from 'src/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FileModule } from 'src/file';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule, FileModule],
+  imports: [UserModule, PostModule, AuthModule, S3Module],
   controllers: [AppController],
   providers: [AppService],
 })
